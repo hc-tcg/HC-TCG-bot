555af8c416a09674ccb1c8c9a784e467371b1905
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2023-05-24 00:03:00 +0100

    Add webhook for game tracking

--- exts/admin.py
+++ exts/admin.py
@@ -1,27 +1,50 @@
-from interactions import Extension, Client, SlashContext, Embed, File, Activity, ActivityType, Timestamp, OptionType, slash_option, slash_command
+from interactions import Extension, Client, SlashContext, Embed, File, Activity, ActivityType, OptionType, slash_option, slash_command
+from aiohttp.web import post, Application, Response, Request
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from interactions.ext.paginators import Paginator
 from datetime import datetime as dt
 from PIL import Image, ImageDraw
+from json import load, dump
+from pyjson5 import decode
 from requests import get
 from io import BytesIO
+from time import time
 
 from datagen import dataGetter
 from deck import deckToHash
 
 class adminExt(Extension):
-    def __init__(self, client:Client, dataGenerator:dataGetter, key:str, url:str, scheduler:AsyncIOScheduler) -> None:
+    def __init__(self, client:Client, dataGenerator:dataGetter, key:str, url:str, scheduler:AsyncIOScheduler, webServer:Application, dataFile:str, counterFile:str) -> None:
         self.dataGen = dataGenerator
         self.headers = {"api-key": key}
         self.url = url
         self.client = client
+        
+        self.dataFile = dataFile
+        try:
+            with open(self.dataFile, "r") as f:
+                self.winData = load(f)
+        except FileNotFoundError:
+            self.winData = []
+
+        self.countFile = counterFile
+        try:
+            with open(self.countFile, "r") as f:
+                self.countData = load(f)
+        except FileNotFoundError:
+            self.countData = []
 
         scheduler.add_job(self.updateStatus, IntervalTrigger(seconds=5))
+
+        webServer.add_routes([post("/game_end", self.gameEnd)])
     
     async def updateStatus(self):
         games:int = len(get(f"{self.url}/games", headers=self.headers).json())
-        await self.client.change_presence(activity=Activity(f"{games} games", ActivityType.WATCHING, "https://hc-tcg-beta.fly.dev/"))
+        self.countData.append([games, round(time(), 2)])
+        with open(self.countFile, "w") as f:
+            dump(self.countData, f)
+        await self.client.change_presence(activity=Activity(f"{games} games", ActivityType.WATCHING, "https://hc-tcg.fly.dev/"))
 
     @slash_command()
     async def admin(self, ctx:SlashContext):
@@ -74,27 +97,34 @@ class adminExt(Extension):
                     im.paste(health, (7*200, i*200), health)
         return im
 
-    def gameEmbed(self, game:dict):
+    def getGameInfo(self, game:dict):
         p1 = game["state"]["players"][game["playerIds"][0]]
-        p1Deck = deckToHash((card["cardId"] for card in p1["pile"]+ p1["hand"] + p1["discarded"])).decode()
+        p1["deck"] = deckToHash((card["cardId"] for card in p1["pile"]+ p1["hand"] + p1["discarded"])).decode()
         p2 = game["state"]["players"][game["playerIds"][1]]
-        p2Deck = deckToHash((card["cardId"] for card in p1["pile"]+ p1["hand"] + p1["discarded"])).decode()
+        p2["deck"] = deckToHash((card["cardId"] for card in p2["pile"]+ p2["hand"] + p2["discarded"])).decode()
+        return p1, p2, {
+            "code": game["code"],
+            "id": game["id"],
+            "creation": game["createdTime"],
+        }
+
+    def gameEmbed(self, game:dict):
+        p1, p2, gameData = self.getGameInfo(game)
         e = Embed(
-            title = f"{game['code']} ({game['id']})" if game["code"] else game["id"],
+            title = f"{gameData['code']} ({gameData['id']})" if gameData["code"] else gameData["id"],
             description=f"{p1['playerName']} ({p1['lives']} lives) vs {p2['playerName']} ({p2['lives']} lives)",
-            timestamp=dt.fromtimestamp(game["createdTime"]/1000),
+            timestamp=dt.fromtimestamp(gameData["creation"]/1000),
         ).set_footer("Bot by Tyrannicodin",
-        ).add_field(f"{p1['playerName']} hash", p1Deck,
-        ).add_field(f"{p2['playerName']} hash", p2Deck,
+        ).add_field(f"{p1['playerName']} hash", p1["deck"],
+        ).add_field(f"{p2['playerName']} hash", p2["deck"],
         ).set_image("attachment://board.png"
         )
         im = self.genBoard(p1["board"], p2["board"])
         return e, im
     
     def simpleInfo(self, game:dict):
-        p1 = game["state"]["players"][game["playerIds"][0]]
-        p2 = game["state"]["players"][game["playerIds"][1]]
-        return f"{game['code']} ({game['id']})" if game["code"] else game["id"], f"{p1['playerName']} ({p1['lives']} lives) vs {p2['playerName']} ({p2['lives']} lives)"
+        p1, p2, gameData = self.getGameInfo(game)
+        return f"{gameData['code']} ({gameData['id']})" if gameData["code"] else gameData["id"], f"{p1['playerName']} ({p1['lives']} lives) vs {p2['playerName']} ({p2['lives']} lives)"
 
     @admin.subcommand()
     @slash_option("search", "The player name, game id or game code to search for", OptionType.STRING)
@@ -104,6 +134,8 @@ class adminExt(Extension):
         data.sort(key = lambda x: x.get("createdTime"))
         if search != "":
             data = next((game for game in data if game["id"] == search or search in game["playerNames"] or game["code"] == search), None)
+            if data:
+                await ctx.send("Couldn't find that player, run `/admin gameinfo` without arguments to get a list of games", ephemeral=True)
             e, im = self.gameEmbed(data)
             with BytesIO() as imBytes:
                 im.save(imBytes, "png")
@@ -125,6 +157,22 @@ class adminExt(Extension):
             await Paginator.create_from_embeds(self.client, embeds, 60).run()
         else:
             await ctx.send(embeds=embeds[0])
+    
+    def addData(self, game:dict):
+        self.winData.append(game)
+        with open(self.dataFile, "w") as f:
+            dump(self.winData, f)
 
-def setup(client, dataGenerator:dataGetter, key:str, url:str, scheduler:AsyncIOScheduler):
-    return adminExt(client, dataGenerator, key, url, scheduler)
+    async def gameEnd(self, req:Request):
+        json:dict = decode((await req.content.read()).decode())
+        if req.headers.get("api-key") != self.headers["api-key"]:
+            return Response(status=403)
+        requiredKeys = ["createdTime", "id", "code", "playerIds", "playerNames", "outcome"]
+        if not all((requiredKey in json.keys() for requiredKey in requiredKeys)):
+            return Response(status=400)
+
+        json["endInfo"].pop("deadPlayerIds")
+        self.addData(json)
+
+def setup(client, dataGenerator:dataGetter, key:str, url:str, scheduler:AsyncIOScheduler, server:Application, dataFile:str, countFile:str):
+    return adminExt(client, dataGenerator, key, url, scheduler, server, dataFile, countFile)
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from interactions import Client
+from aiohttp.web import Application, AppRunner, TCPSite
+from interactions import Client, listen
 
 from datagen import dataGetter
 
-bot = Client()
+class Bot(Client):
+    @listen()
+    async def on_ready(event):
+        await runner.setup()
+        site = TCPSite(runner, "127.0.0.1", 80)
+        await site.start()
+    
+    @listen()
+    async def on_disconnect(event):
+        await runner.cleanup()
+
+bot = Bot()
 
 API_URL = "https://hc-tcg.fly.dev/api"
 DOTD_PATH = "dotd.json"
+WIN_DATA = "games.json"
+COUNT_DATA = "count.json"
 
 with open("token.txt", "r",) as f:
     lines = f.readlines()
@@ -17,15 +31,23 @@ with open("token.txt", "r",) as f:
 dataGen = dataGetter(gitToken)
 scheduler = AsyncIOScheduler()
 
-@bot.event()
-async def on_ready():
-    scheduler.start()
+webServer = Application()
+runner = AppRunner(webServer)
 
 bot.load_extension("exts.card", None, dataGenerator=dataGen)
 bot.load_extension("exts.util", None)
-bot.load_extension("exts.admin", None, dataGenerator=dataGen, key=tcgToken, url=API_URL, scheduler=scheduler)
+bot.load_extension("exts.admin", None,
+                   dataGenerator=dataGen,
+                   key=tcgToken,
+                   url=API_URL,
+                   scheduler=scheduler,
+                   server=webServer,
+                   dataFile=WIN_DATA,
+                   countFile=COUNT_DATA,
+                  )
 bot.load_extension("exts.dotd", None, fp=DOTD_PATH)
 
 print("Bot running!")
 
+scheduler.start()
 bot.start(botToken)
