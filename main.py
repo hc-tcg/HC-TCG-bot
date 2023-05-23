0012e31d8d9f9f8224f77db97a9406ea57f9bcf9
Tyrannicodin <86689800+Tyrannicodin@users.noreply.github.com>
2023-05-23 13:46:50 +0100

    Update api url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datagen import dataGetter
 
 bot = Client()
 
-API_URL = "https://hc-tcg-beta.fly.dev/api"
+API_URL = "https://hc-tcg.fly.dev/api"
 DOTD_PATH = "dotd.json"
 
 with open("token.txt", "r",) as f:
