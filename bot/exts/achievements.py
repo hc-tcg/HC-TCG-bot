803c0419b784be3f93e632a209342baa01d1eaf7
Tyrannicodin <tyrannicodin@gmail.com>
2025-03-05 22:07:46 +0000

    Add achievement commands

---
+++ bot/exts/achievements.py
@@ -0,0 +1,156 @@
+"""Get information about cards and decks."""
+
+from __future__ import annotations
+
+from collections import Counter
+from collections.abc import Iterable
+from datetime import datetime as dt
+from datetime import timezone
+from itertools import islice
+from math import ceil, sqrt
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from interactions import (
+    AutocompleteContext,
+    Client,
+    Embed,
+    Extension,
+    OptionType,
+    SlashContext,
+    global_autocomplete,
+    slash_command,
+    slash_option,
+)
+
+from bot.util import ServerManager, rgb_to_int
+from bot.util.datagen import hex_to_int
+
+
+def take(items: int, iterable: Iterable) -> list:
+    """Return first `items` items of the iterable as a list."""
+    return list(islice(iterable, items))
+
+
+beige = (226, 202, 139)
+
+
+def count(s: str) -> str:
+    """Count the number of items required."""
+    final = []
+    for k, v in Counter(s).most_common():
+        final.append(f"{v}x {k}")
+    return ", ".join(final) if len(final) else "None"
+
+
+def best_factors(number: int) -> tuple[int, int]:
+    """Get as close to being square as possible."""
+    x = sqrt(number) // 1
+    return ceil(x), ceil(x if number - x**2 == 0 else (number - x**2) / x + x)
+
+
+class AchievementExt(Extension):
+    """Get information about cards and decks."""
+
+    def __init__(
+        self: AchievementExt,
+        _: Client,
+        manager: ServerManager,
+        _scheduler: AsyncIOScheduler,
+    ) -> None:
+        """Get information about cards and decks.
+
+        Args:
+        ----
+        client (Client): The discord bot client
+        manager (ServerManager): The server connection manager
+        _scheduler (AsyncIOScheduler): Event scheduler
+        """
+        self.manager: ServerManager = manager
+
+    @global_autocomplete("achievement_name")
+    async def achievement_autocomplete(self: AchievementExt, ctx: AutocompleteContext) -> None:
+        """Autocomplete an achievement name."""
+        server = self.manager.get_server(ctx.guild_id)
+        if not ctx.input_text:
+            await ctx.send(
+                [
+                    achievement.name
+                    for achievement in take(25, server.data_generator.achievement_universe)
+                ]
+            )
+            return
+        await ctx.send(
+            [
+                card.name
+                for card in server.data_generator.achievement_universe
+                if ctx.input_text.lower() in card.name.lower()
+            ][0:25]
+        )
+
+    @slash_command()
+    async def achievement(self: AchievementExt, _: SlashContext) -> None:
+        """Get information about achievements."""
+
+    @achievement.subcommand()
+    @slash_option(
+        "achievement_name",
+        "The achievement to get",
+        OptionType.STRING,
+        required=True,
+        autocomplete=True,
+    )
+    @slash_option("uuid", "Player uuid to get progress of", OptionType.STRING)
+    async def info(
+        self: AchievementExt, ctx: SlashContext, achievement_name: str, uuid: str = ""
+    ) -> None:
+        """Get information about cards and decks."""
+        server = self.manager.get_server(ctx.guild_id)
+        achievements = [
+            achievement
+            for achievement in server.data_generator.achievement_universe
+            if achievement_name.lower() in achievement.name.lower()
+        ]
+        achievements.sort(key=lambda val: val.name)
+        if len(achievements) > 0:
+            achievement = achievements[0]
+            color: int = 0
+            if achievement.border_color:
+                color = hex_to_int(achievement.border_color)
+            e = Embed(
+                title=achievement.name,
+                description=achievement.description,
+                timestamp=dt.now(tz=timezone.utc),
+                color=color,
+            )
+            if uuid != "":
+                progress = await server.get_player_achievement_progress(uuid, achievement)
+                e.add_field("Progress", f"{progress}/{achievement.steps}")
+            else:
+                e.add_field("Steps", str(achievement.steps), inline=True)
+            e.add_field(
+                "Global progress",
+                f"{await server.get_global_achievement_progress(achievement)}%",
+                inline=True,
+            )
+            if achievement.image_url:
+                e.set_thumbnail(achievement.image_url)
+            e.set_footer("Bot by Tyrannicodin16")
+            await ctx.send(embeds=e)
+        else:
+            await ctx.send("Couldn't find that card!", ephemeral=True)
+
+
+def setup(
+    client: Client,
+    manager: ServerManager,
+    scheduler: AsyncIOScheduler,
+) -> Extension:
+    """Create the extension.
+
+    Args:
+    ----
+    client (Client): The discord bot client
+    manager (ServerManager): The server connection manager
+    scheduler (AsyncIOScheduler): Event scheduler
+    """
+    return AchievementExt(client, manager, scheduler)
--- bot/util/datagen.py
+++ bot/util/datagen.py
@@ -42,19 +42,27 @@ def rgb_to_int(rgb: tuple[int, int, int]) -> int:
     return (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]
 
 
-class Card:
-    """Basic image generator for a card."""
+def hex_to_int(hex_str: str) -> int:
+    """Convert an hex string to an integer.
 
-    def __init__(self: Card, data: dict, generator: DataGenerator) -> None:
+    Args:
+    ----
+    hex_str (tuple): hex color to convert
+    """
+    return int(hex_str.lstrip("#"), 16)
+
+
+class Card:
+    """Card data."""
+
+    def __init__(self: Card, data: dict) -> None:
         """Init card.
 
         Args:
         ----
         data (dict): card informtaion
-        generator (dict): generator this card is part of
         """
         self._raw_data: dict = data
-        self.generator: DataGenerator = generator
 
         self.text_id: str = data["id"]
         self.category: str = data["category"]
@@ -71,9 +79,9 @@ class Card:
 
 
 class HermitCard(Card):
-    """Image creator for a hermit card."""
+    """Hermit card data."""
 
-    def __init__(self: HermitCard, data: dict, generator: DataGenerator) -> None:
+    def __init__(self: HermitCard, data: dict) -> None:
         """Init card.
 
         Args:
@@ -81,7 +89,7 @@ class HermitCard(Card):
         data (dict): card informtaion
         generator (dict): generator this card is part of
         """
-        super().__init__(data, generator)
+        super().__init__(data)
 
         self.hermit_type: str = data["type"]
         self.health: int = data["health"]
@@ -89,9 +97,9 @@ class HermitCard(Card):
 
 
 class EffectCard(Card):
-    """Image creator for an effect card."""
+    """Effect card data."""
 
-    def __init__(self: EffectCard, data: dict, generator: DataGenerator) -> None:
+    def __init__(self: EffectCard, data: dict) -> None:
         """Init card.
 
         Args:
@@ -99,15 +107,15 @@ class EffectCard(Card):
         data (dict): card informtaion
         generator (dict): generator this card is part of
         """
-        super().__init__(data, generator)
+        super().__init__(data)
 
         self.description = data["description"]
 
 
 class ItemCard(Card):
-    """Image creator for an item card."""
+    """Item card data."""
 
-    def __init__(self: ItemCard, data: dict, generator: DataGenerator) -> None:
+    def __init__(self: ItemCard, data: dict) -> None:
         """Init card.
 
         Args:
@@ -117,17 +125,46 @@ class ItemCard(Card):
         """
         self.energy: list[str] = data["energy"]
 
-        super().__init__(data, generator)
+        super().__init__(data)
 
 
-def get_card(data: dict, data_generator: DataGenerator) -> Card:
+class Achievement:
+    """Achievement data."""
+
+    def __init__(self: Achievement, data: dict) -> None:
+        """Init achievement.
+
+        Args:
+        ----
+        data (dict): achievement informtaion
+        generator (dict): generator this achievement is part of
+        """
+        self._raw_data: dict = data
+
+        self.achievement_id: str = data["achievementId"]
+        self.name: str = data["name"]
+        self.description: str = data["description"]
+        self.steps: int = data["steps"]
+
+        self.index: int = data["index"]
+        self.max_level: int = data["maxIndex"]
+        self.image_url: str | None = None
+        self.border_color: str | None = None
+        if data["preview"]:
+            self.image_url = data["preview"]["image"]
+            self.border_color = (
+                data["preview"]["borderColor"] if "borderColor" in data["preview"].keys() else None
+            )
+
+
+def get_card(data: dict) -> Card:
     """Create a card class of the correct type."""
     if data["category"] == "hermit":
-        return HermitCard(data, data_generator)
+        return HermitCard(data)
     if data["category"] == "attach" or data["category"] == "single_use":
-        return EffectCard(data, data_generator)
+        return EffectCard(data)
     if data["category"] == "item":
-        return ItemCard(data, data_generator)
+        return ItemCard(data)
     invalid_folder = "Invalid category: " + data["category"]
     raise ValueError(invalid_folder)
 
@@ -144,10 +181,13 @@ class DataGenerator:
     async def reload_all(self: DataGenerator) -> None:
         """Reload all card information."""
         self.cache: dict[str, Image.Image] = {}
-        self.universe: dict[str, Card] = {}
+        self.card_universe: dict[str, Card] = {}
+        self.achievement_universe: list[Achievement] = []
 
-        for card in await self.load_data():
-            self.universe[card.text_id] = card
+        for card in await self.load_cards():
+            self.card_universe[card.text_id] = card
+        for achievement in await self.load_achievements():
+            self.achievement_universe.append(achievement)
 
     async def get_image(self: DataGenerator, path: str) -> Image.Image:
         """Get an image from the server.
@@ -173,7 +213,7 @@ class DataGenerator:
         except Image.UnidentifiedImageError:
             return Image.new("RGBA", (0, 0))
 
-    async def load_data(self: DataGenerator) -> list[Card]:
+    async def load_cards(self: DataGenerator) -> list[Card]:
         """Load all card data."""
         cards = []
         async with self.http_session.get("cards") as response:
@@ -186,5 +226,21 @@ class DataGenerator:
             iterator = tqdm(iterator, "Loading cards")
 
         for card in iterator:
-            cards.append(get_card(card, self))
+            cards.append(get_card(card))
         return cards
+
+    async def load_achievements(self: DataGenerator) -> list[Achievement]:
+        """Load all achievement data."""
+        achievements = []
+        async with self.http_session.get("achievements") as response:
+            content = await response.content.read()
+            if not response.ok:
+                return []
+
+        iterator = loads(content.decode())
+        if has_progression:
+            iterator = tqdm(iterator, "Loading achievements")
+
+        for achievement in iterator:
+            achievements.append(Achievement(achievement))
+        return achievements
